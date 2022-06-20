a6b3bbaf2278976af2c2a8af6cdd838a2f8fb129
Raja Das <rdas6313@gmail.com>
2022-06-20 20:18:20 +0530

    refactor: logging change with environment

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,7 +1,8 @@
 import config from '../configs/tic_tac_toe/config.json';
 
 function log(msg1, msg2 = null) {
-    if(config.env && config.env === "dev")
+   
+    if(process.env.NODE_ENV === config.env)
         console.log(msg1, msg2);
 }
 
